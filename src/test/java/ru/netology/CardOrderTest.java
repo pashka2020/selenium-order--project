cef98dc36f1c09c5aaa8ac5e67e407fc79a37f4b
Neto test <pashtet201807@gmail.com>
2026-06-24 09:17:57 +0300

    initial commit

---
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -0,0 +1,58 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless"); // Обязательно для работы в GitHub Actions
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit(); // Закрываем браузер после каждого теста
+        }
+    }
+
+    @Test
+    void shouldSubmitRequestSuccessfully() {
+
+        driver.get("http://localhost:9999");
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иван Петров-Сидоров");
+
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79991234567");
+
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+
+        driver.findElement(By.cssSelector("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+}
